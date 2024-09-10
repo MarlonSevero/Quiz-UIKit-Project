0d800cc3683f9cb2fd0b6573d078903853078fba
Marlon Salles Severo <marlonsallessevero@hotmail.com>
2024-09-09 22:17:45 -0300

    IQuiz HomePage

---
+++ iQuiz/iQuiz/001_explicando.swift
@@ -0,0 +1,16 @@
+//
+//  Descricao.swift
+//  iQuiz
+//
+//  Created by Marlon Severo on 09/09/24.
+//
+
+/*
+ Arquivo Main : É o storyboard da aplicacao.
+ Storyboard: E o conjunto de todas as telas de uma aplicacao. Representa todo o fluxo do aplicativo.
+ E utilizado com Drag 'n' Drap (Arrastar e Soltar)
+
+ Assets: Onde sera colocado todas as imagens da aplicacao.
+ 
+ Cada tela do Storyboard tera uma ViewController
+ */
---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Marlon Severo on 09/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
